05430d50ec8afc7b0af2ffd78a4fe2b7f11b02b5
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-02-20 00:08:49 -0600

    updated connection to use dotenv

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,12 +5,13 @@ require('dotenv').config();
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     // Your MySQL username,
-    user: 'root',
+    user: process.env.DB_USER,
     // Your MySQL password
     password: process.env.DB_PW,
-    database: 'training'
+    // databse name
+    database: process.env.DB_NAME
   },
   console.log('Connected to the election database.')
 );
